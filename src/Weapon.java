db4d2ef28d42fdb441fecfc471acd6cfc12ca33d
AsanalievTemirlan <tonyadam050607@gmail.com>
2023-12-20 00:17:34 +0600

    Чтобы работало

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Weapon {
+public class Weapon {
+    private WeaponType weaponType;
+    private String nameOfTheWeapon;
+
+    public WeaponType getWeaponType() {
+        return weaponType;
+    }
+
+    public void setWeaponType(WeaponType weaponType) {
+        this.weaponType = weaponType;
+    }
+
+    public String getNameOfTheWeapon() {
+        return nameOfTheWeapon;
+    }
+
+    public void setNameOfTheWeapon(String nameOfTheWeapon) {
+        this.nameOfTheWeapon = nameOfTheWeapon;
+    }
 }
